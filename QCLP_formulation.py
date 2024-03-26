f4edcbfb8bdc8454a2326b7eda435f6d68ec5650
Abdelmalik Benfadhil <malikbf5@gmail.com>
2024-03-26 15:33:25 +0100

    Add files via upload

--- QCLP_formulation.py
+++ QCLP_formulation.py
@@ -55,3 +55,61 @@ def qclp_formulation(num_states, num_actions, num_observations, num_nodes, b_0, 
                                 * model.y[qp_, sp_] for qp_ in model.q for o_ in model.o for sp_ in model.s ]) for a_ in model.a]))
                 
         return model
+    
+
+
+
+
+# initialize the model variable x with win_stay lose_shift strategy for two nodes controller
+def win_stay_lose_shift(num_nodes,newmodel):
+    # initial values for the variables with Win-Stay Lose-Shift policy
+    if num_nodes == 2:
+        for qnode in newmodel.q:
+            for actionn in newmodel.a:
+                for qnodeprime in newmodel.q:
+                    for obs in newmodel.o:
+                        if qnode == qnodeprime and qnode == actionn and obs == 1:
+                            newmodel.x[qnodeprime,actionn,qnode,obs] = 1
+                            # print(f"being in {qnode} having chosen {actionn} and observed {obs} we stay in {qnodeprime}", newmodel.x[qnodeprime,actionn,qnode,obs].value)
+                        elif qnode != qnodeprime and qnode == actionn and obs == 0:
+                            newmodel.x[qnodeprime,actionn,qnode,obs] = 1 
+                            # print(f"being in {qnode} having chosen {actionn} and observed {obs} we shift to {qnodeprime}",newmodel.x[qnodeprime,actionn,qnode,obs].value)
+                        else:
+                            newmodel.x[qnodeprime,actionn,qnode,obs] = 0
+                            # print(f"being in {qnode} having chosen {actionn} and observed {obs} we don't go to {qnodeprime}",newmodel.x[qnodeprime,actionn,qnode,obs].value)
+        newmodel.x.pprint() 
+
+
+
+
+
+# initialize the model variable x with random strategy
+def generate_randomx(model,num_nodes,num_actions):
+    for qnode in model.q:
+        # left in P(a | q)
+        left_in_a = 1
+        for actionn in model.a:
+            # if last a
+            if actionn == num_actions - 1:
+                prob_a = left_in_a
+            # else generate random number between 0 and what's left
+            else:
+                prob_a = random.uniform(0,left_in_a)
+                left_in_a -= prob_a
+            for obs in model.o:
+            # left in P(q' | q, a, o)
+                left_in_qp = 1
+                for qnodeprime in model.q:
+                    # if last q'
+                    if qnodeprime == num_nodes - 1:
+                        prob_qp = left_in_qp
+                    # else generate random number between 0 and what's left
+                    else:
+                        prob_qp = random.uniform(0,left_in_qp)
+                        left_in_qp -= prob_qp
+                    # P(q',a | q,o) = P(q' | q, a, o) * P(a | q)
+                    model.x[qnodeprime,actionn,qnode,obs] = prob_qp * prob_a
+
+
+
+
